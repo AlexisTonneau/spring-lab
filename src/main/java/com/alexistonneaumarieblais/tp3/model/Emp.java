74356e08d70428e75be8b53a065a4587da1cc8e7
Alexis Tonneau <atonneau@juniorisep.com>
2021-04-07 15:42:31 +0200

    List working

--- src/main/java/com/alexistonneaumarieblais/tp3/model/Emp.java
+++ src/main/java/com/alexistonneaumarieblais/tp3/model/Emp.java
@@ -1,11 +1,14 @@
 package com.alexistonneaumarieblais.tp3.model;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Emp {
     @Id
     @GeneratedValue
